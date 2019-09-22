59d29cc9dd19a5752827f425cedfd50d1dc48730
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-22 21:35:05 +0000

    Deploy embalex/table to github.com/embalex/table.git:gh-pages

---
+++ precache-manifest.8c52e24a8d4b048ab66f2b1c965692ef.js
@@ -0,0 +1,27 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28671aae27f06f63c7ddb60e98fd229",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.6757a5aa75e07bb60471.bundle.js"
+  },
+  {
+    "revision": "e5a369382e0fb61c6f9d",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.6757a5aa75e07bb60471.bundle.js"
+  },
+  {
+    "revision": "0a78fba7fc42277c43ce4038652f9d9a",
+    "url": "static/media/LabGrotesque-Regular.0a78fba7.otf"
+  },
+  {
+    "revision": "95a64198167675231223403a9fe35d6d",
+    "url": "static/media/Roobert-Regular.95a64198.otf"
+  },
+  {
+    "url": "vendors~main.6757a5aa75e07bb60471.bundle.js"
+  }
+]);
